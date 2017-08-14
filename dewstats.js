bc075a445cf297ce801a927ff4f301b3cfd416ad
medsouz <medsouz99@gmail.com>
2017-08-14 11:42:26 -0400

    Add error handling, log more data

--- dewstats.js
+++ dewstats.js
@@ -22,6 +22,12 @@ const Server = sequelize.define('Server', {
 	hostName: {
 		type: Sequelize.TEXT
 	},
+	port: {
+		type: Sequelize.INTEGER
+	},
+	password: {
+		type: Sequelize.BOOLEAN
+	},
 	dedicated: {
 		type: Sequelize.BOOLEAN
 	},
@@ -31,6 +37,15 @@ const Server = sequelize.define('Server', {
 	maxPlayers: {
 		type: Sequelize.INTEGER
 	},
+	eldewritoVersion: {
+		type: Sequelize.TEXT
+	},
+	sprintEnabled: {
+		type: Sequelize.BOOLEAN
+	},
+	assassinationEnabled: {
+		type: Sequelize.BOOLEAN
+	}
 
 });
 
@@ -59,24 +74,32 @@ requestify.get(MASTERSERVER).then(function(response) {
 	if(body.listVersion !== undefined && body.listVersion == "1") {
 		var list = body.result.servers;
 		return Promise.map(list, function(item, index, length) {
-			return requestify.get("http://" + item, {timeout: 3000}).then(function(response){
+			return requestify.get("http://" + item, {timeout: 3000}).catch(function() {/* ignore connection errors */}).then(function(response){
+				// If a server fails to respond then return null
+				if(!response)
+					return null;
 				var server = response.getBody();
 				if(server.name !== undefined) {
 					server.ip = item;
 					return server;
 				}
 			});
-		}).then(function(result) {
+		}).catch(function(err) {console.log(err)}).then(function(result) {
+			//Remove servers that failed to load
+			result = result.filter(function(item) {
+				return item !== null;
+			});
+
 			var totalPlayers = 0;
 			var totalCapacity = 0;
 			// Precalculate the totals to add to the scan object
 			for(var i in result) {
 				var server = result[i];
-
 				totalPlayers += server.numPlayers;
 				totalCapacity += server.maxPlayers;
 			}
 
+			console.log("Found " + result.length + " servers");
 			console.log("Total Players: " + totalPlayers);
 			console.log("Total Capacity: " + totalCapacity);
 
@@ -90,9 +113,14 @@ requestify.get(MASTERSERVER).then(function(response) {
 							ip: server.ip,
 							serverName: server.name,
 							hostName: server.hostPlayer,
+							port: server.port,
+							password: (server.xnaddr === undefined),
 							dedicated: (server.isDedicated !== undefined) ? server.isDedicated : false, //Not implemented in 0.5.1.1
 							numPlayers: server.numPlayers,
 							maxPlayers: server.maxPlayers,
+							eldewritoVersion: server.eldewritoVersion,
+							sprintEnabled: server.sprintEnabled,
+							assassinationEnabled: server.assassinationEnabled,
 							ScanId: scan.id
 						});
 					});
